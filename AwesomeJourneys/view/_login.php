79be98e69795cfb7322835db22be1f02d3e17d30
Fabio <Fabio@isengard>
2014-01-19 21:58:26 +0100

    modifica del menù di login, rimozione del carattere |

--- AwesomeJourneys/view/_login.php
+++ AwesomeJourneys/view/_login.php
@@ -1,8 +1,8 @@
 <?php 
     if(isset($user)){
-        echo "<div id='div_login'><p>".$user->getRole()." profile | <a href='index.php?op=logout'>Logout</a></p></div>";
+        echo "<div id='div_login'><p>".$user->getRole()." profile  <a href='index.php?op=logout'>Logout</a></p></div>";
     }
     else{
-        echo "<div id='div_login'><p><a href='index.php?op=login'>Area clienti</a> | <a href='index.php?op=register'>Registrati</a></p></div>";
+        echo "<div id='div_login'><p><a href='index.php?op=login'>Area clienti</a>  <a href='index.php?op=register'>Registrati</a></p></div>";
     }
 ?>
