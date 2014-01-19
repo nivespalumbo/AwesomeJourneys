9d5542a9a01307437b081162141e04725100fb86
Maria Nives Palumbo <nives.palumbo@outlook.com>
2014-01-19 16:33:59 +0100

    documentazione

--- AwesomeJourneys/view/itinerary.php
+++ AwesomeJourneys/view/itinerary.php
@@ -1,7 +1,15 @@
 <?php
-include_once 'Oggetti/ItineraryInHTML.php';
+//include_once 'Oggetti/ItineraryInHTML.php';
+//
+//$view = new ItineraryInHTML($this->model);
+//$view->getItinerary();
 
-$view = new ItineraryInHTML($this->model);
-$view->getItinerary();
+?>
+
+<h2><?php echo $this->model->getName(); ?></h2>
+<p><?php echo $this->model->getDescription(); ?></p>
+
+<h3>Tappe</h3>
+<?php
 
 ?>
