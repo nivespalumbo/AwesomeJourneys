350d2c2a1d47901dead1c3a5150af6d69bbd54c8
Fabio <Fabio@isengard>
2014-01-24 15:25:54 +0100

    stile itinerary

--- AwesomeJourneys/view/itinerary.php
+++ AwesomeJourneys/view/itinerary.php
@@ -1,53 +1,53 @@
 <?php $user = unserialize($_SESSION['utente']); ?>
-
+<?php 
+include_once '_login.php';
+include_once '_personalmenu.php' 
+?>
+<div id="my_itineray">
 <h2><?php echo $this->model->getName(); ?></h2>
 <p><?php echo $this->model->getDescription(); ?></p>
-
+</div>
+<div id="stage">
 <h3>Tappe</h3><a href="index.php?op=searchStay" >Search stay</a>
+</div>
 
-<form action="index.php" method="POST">
-<div>
+<div id="stages">
     <?php
     $tappe = $this->model->getItineraryBricks();
     foreach($tappe as $tappa){
-        echo "<div>"
+        echo "<div id='select_stages'>"
             . "<label>".$tappa->getTemplate()->getName()."</label>"
             . "<p>".$tappa->getTemplate()->getDescription()."</p>"
             . "<div>"
-            . "<h3>Attività</h3>"; 
-        echo "<ul>";
+            . "<div class='activity_title'><h3>AttivitÃ </h3></div>"; 
         $attivitaPreviste = $tappa->getActivities();
         $attivitaSelezionate = $tappa->getSelectedActivities();
         foreach($attivitaPreviste as $attivita){
-            echo "<li>"
+            echo "<div class='activity'>"
                  . "<input type='checkbox' name='idActivity[]' value='".$attivita->getId()."'";
             if(array_key_exists($attivita->getId(), $attivitaSelezionate)){ echo "checked" ; }
             echo " />";
             echo "<b>".$attivita->getName()."</b>"
                  . "<p>".$attivita->getDescription()."</p>"
-                 . "</li>";
+                 ."</div>";
         }
-        echo "</ul>"
-            . "</div>";
+        echo"</div>";
         
         echo "<div>
-                <h3>Pernottamenti disponibili</h3>
-                <ul>";
+                    <div class='activity_title'><h3>Pernottamenti disponibili</h3></div>                ";
         $accomodations = $tappa->getAccomodations();
         foreach($accomodations as $a){
-            echo "<li>"
+            echo "<div class='activity'>"
                  . "<input type='radio' name='idAccomodation' value='".$a->getId()."'";
             if($a->getId() == $tappa->getSelectedAccomodation()){ echo "checked" ; }
             echo " />";
             echo "<b>".$a->getName()."</b>"
                  . "<div><span>Tipo: ".$a->getAccomodationType()."</span>"
                  . "<span>Location: ".$a->getLocation()."</span></div>"
-                 . "</li>";
+                 . "</div>";
         }
         echo "</div></div>";
     }
     ?>
     </ul>
 </div>
-<button type="submit" name="op" value="saveItinerary">Salva</button>
-</form>
